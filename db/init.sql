080297e1bce991d6a11021fe984ab150470db03c
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-05-28 02:33:35 +0700

    finsihed: init.sql

--- db/init.sql
+++ db/init.sql
@@ -7,3 +7,35 @@ CREATE TABLE IF NOT EXISTS Users (
     last_login TIMESTAMP
 );
 
+CREATE TABLE IF NOT EXISTS DailyLogs (
+    log_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    content TEXT NOT NULL,
+    date DATE NOT NULL DEFAULT CURRENT_DATE,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+CREATE TYPE status_type AS ENUM ('pending', 'completed');
+
+CREATE TABLE IF NOT EXISTS ToDoLists (
+    todo_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    description TEXT,
+    due_date DATE,
+    priority INT NOT NULL DEFAULT 0,
+    status status_type NOT NULL DEFAULT 'pending',
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS CalendarEvents (
+    event_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    description TEXT,
+    start_date TIMESTAMP NOT NULL,
+    end_date TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
