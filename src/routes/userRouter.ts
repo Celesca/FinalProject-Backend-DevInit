1ba6163acc7eb66388952435eff1fccb6bf8a4ee
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-05-29 23:55:45 +0700

    add: user login

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -5,3 +5,7 @@ export const userRouter = Router();
 userRouter.post("/register", async (req: Request, res: Response) => {
   res.send("Register route");
 });
+
+userRouter.post("/login", async (req: Request, res: Response) => {
+  res.send("Login route");
+});
